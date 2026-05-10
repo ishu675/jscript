35f413fa793a6609a413db28bd6447b43bde40d5
ishu675 <dasitishree524@gmail.com>
2026-05-10 15:30:42 +0000

    datatype summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 13246789787553423n
+
+const heros = ["sa", "de", "ko"]
+let myObj = {
+    name: "ishu",
+    age: 24,
+}
+
+const myFunction= function(){
+    console.log("hii");
+}
+
+console.log(typeof anotherId);
