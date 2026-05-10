e812b68cbba67ad362a511bb599e5ea3f83e49f6
ishu675 <dasitishree524@gmail.com>
2026-05-10 10:15:16 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "ishu@google.com"
+var accountPassword = "12345"
+accountCity = "cuttack"
+let accountState;
+
+// accountId = 2 //Not allowed
+
+accountEmail = "itu@google.com"
+accountPassword = "121212"
+accountCity = "bbsr"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
