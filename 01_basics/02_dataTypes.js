73c394a978ec293d3073ec0307b10ef5d23d5256
ishu675 <dasitishree524@gmail.com>
2026-05-10 10:34:40 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+console.log(3+3)
+console.log("ishu");
+let name = "ishu"
+let age = 24
+let isloggedin=false
+let state;
+
+console.log(typeof "ishu");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
